303211819e3ca6b35296f354c614a17336428812
Md. Waliul Islam Rayhan <b190305034@cse.jnu.ac.bd>
2024-08-31 02:51:57 +0600

    Collaborative Editor Room

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,13 +1,40 @@
-import { Editor } from '@/components/editor/Editor'
-import Header from '@/components/Header'
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import CollaborativeRoom from "@/components/CollaborativeRoom"
+import { getDocument } from "@/lib/actions/room.action";
+import { getClerkUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation";
 
-import React from 'react'
+const Document = async ({ params: { id } }: SearchParamProps) => {
+  const clerkUser = await currentUser();
+  if(!clerkUser) redirect('/sign-in');
 
-const Document = () => {
+  const room = await getDocument({
+    roomId: id,
+    userId: clerkUser.emailAddresses[0].emailAddress,
+  });
+
+  if(!room) redirect('/');
+
+  // const userIds = Object.keys(room.usersAccesses);
+  // const users = await getClerkUsers({ userIds });
+
+  // const usersData = users.map((user: User) => ({
+  //   ...user,
+  //   userType: room.usersAccesses[user.email]?.includes('room:write')
+  //     ? 'editor'
+  //     : 'viewer'
+  // }))
+
+  // const currentUserType = room.usersAccesses[clerkUser.emailAddresses[0].emailAddress]?.includes('room:write') ? 'editor' : 'viewer';
+
   return (
-    <div>
-    </div>
+    <main className="flex w-full flex-col items-center">
+      <CollaborativeRoom 
+        roomId={id}
+        roomMetadata={room.metadata} users={[]} currentUserType={"creator"}        // users={usersData}
+        // currentUserType={currentUserType}
+      />
+    </main>
   )
 }
 
--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -7,15 +7,19 @@ import {
 import { Editor } from '@/components/editor/Editor'
 import Header from '@/components/Header'
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import ActiveCollaborators from "@/components/ActiveCollaborators";
 
-const CollaborativeRoom = () => {
+const CollaborativeRoom = ({ roomId, roomMetadata }: CollaborativeRoomProps) => {
     return (
-        <RoomProvider id="my-room">
+        <RoomProvider id={roomId}>
             <ClientSideSuspense fallback={<div>Loading…</div>}>
                 <div className="collaborative-room">
                     <Header>
                         <div className="flex w-fit items-center justify-center gap-2">
                             <p className="document-title">Document</p>
+                        </div>
+                        <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
+                            <ActiveCollaborators />
                         </div>
                         <SignedOut>
                             <SignInButton />
